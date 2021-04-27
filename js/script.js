2075502e6a0ad51d1eec80e9e99c8cd0fb9718ac
marcomanelli <tronmsi@gmail.com>
2021-04-27 15:48:36 +0200

    a

--- js/script.js
+++ js/script.js
@@ -1,33 +1,86 @@
-$(function() {
-  var numComputer = []
-  var numGiocatore = []
+var arrRandom, arrUser, arrResult;
+var limit = 5;
+var secAttesa = 2;
 
-  $('#start').click(function(){
-    $(this).slideUp()
+$(function () {
+  reset();
 
-    for(var i=0; i<5; i++){
-      numComputer[i] = Math.floor(Math.random() * 100 + 1)
+  $("#start").click(function () {
+    while (arrRandom.length < limit) {
+      var nr = getNumberGenerator(1, 100);
 
+      if (!arrRandom.includes(nr)) arrRandom.push(nr);
     }
-    $('#numeri').text(numComputer)
 
-    setTimeout(function(){
-     $('#numeri').hide()
-    }, 5000)
-  })
+    printDisplay("I numeri estratti sono: " + arrRandom.join(" - "));
+    $("#start").hide();
 
-  numGiocatore.push($('#input').value())
+    setTimeout(function () {
+      printDisplay("Inserisci " + limit + " numeri.");
+      $("#console").show();
+    }, secAttesa * 1000);
+  });
 
-  console.log(numGiocatore);
-  /* //inserimento numeri giocatore
-  for(var i=0; i<5; i++){
-    numGiocatore = parseInt(prompt('Inserisci i numeri uno alla volta'))
-  }
-  setTimeout(function(){
-    $('#attesa').show()
-   }, 5000)
+  $("#send-number").click(function () {
+    if (arrUser.length < limit) {
+      if (arrUser.includes($("input").val())) {
+        printDisplay("Attenzione numero già inserito");
+      } else {
+        arrUser.push($("input").val());
+        printDisplay("Numeri inseriti: " + arrUser.join(" - "));
+      }
+    }
+    if (arrUser.length == limit) {
+      for (var num of arrUser) {
+        if (arrRandom.includes(parseInt(num))) {
+          arrResult.push(num);
+        }
+      }
+      console.log(arrResult);
 
-  setTimeout(function(){
-    $('#attesa').hide()
-  }, 8000) */
-})
+      setTimeout(function () {
+        printDisplay("Attesa...");
+        $("#console").hide();
+      }, 500);
+
+      setTimeout(function () {
+        if (arrResult.length === 0) {
+          printDisplay("Hai perso!");
+        } else if (arrResult.length === limit) {
+          printDisplay("Hai vinto");
+        } else {
+          printDisplay(
+            "Hai indovinato questi numeri: " + arrResult.join(" - ")
+          );
+        }
+        $("#restart").show();
+      }, 2500);
+    }
+    $("input").val("");
+    $("input").focus();
+  });
+
+  $("#restart").click(function () {
+    reset();
+  });
+});
+
+function reset() {
+  limit = parseInt(prompt("Scegli quanti numeri inserire"));
+  arrRandom = [];
+  arrUser = [];
+  arrResult = [];
+  printDisplay("Per iniziare a giocare clicca su VIA");
+  $("#display").show();
+  $("#start").show();
+  $("#restart").hide();
+  $("#console").hide();
+}
+
+function printDisplay(text) {
+  $("#display").text(text);
+}
+
+function getNumberGenerator(min, max) {
+  return Math.floor(Math.random() * (max - min) + 1) + min;
+}
